bdd0aad0e6c0bf679de761d9917b959aa6382ca7
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2021-11-19 04:29:00 -0300

    Criada classe com os steps.

---
+++ bdd-cucumber/src/test/java/me/brunosantana/bdd/acceptance/steps/PropondoLancesSteps.java
@@ -0,0 +1,27 @@
+package me.brunosantana.bdd.acceptance.steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class PropondoLancesSteps {
+	
+	@Given("Dado um lance valido")
+	public void dado_um_lance_valido() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	
+	@When("Quando propoe ao leilao")
+	public void quando_propoe_ao_leilao() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	
+	@Then("Entao o lance eh aceito")
+	public void entao_o_lance_eh_aceito() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+}
