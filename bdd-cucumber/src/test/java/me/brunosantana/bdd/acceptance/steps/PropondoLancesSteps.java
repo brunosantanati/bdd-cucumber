ca6aa4c45ed6c1e7eb001daed38e95e6b1b0b299
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2021-11-19 04:34:07 -0300

    Implementacao dos steps do cenario de 1 unico lance valido.

--- bdd-cucumber/src/test/java/me/brunosantana/bdd/acceptance/steps/PropondoLancesSteps.java
+++ bdd-cucumber/src/test/java/me/brunosantana/bdd/acceptance/steps/PropondoLancesSteps.java
@@ -1,27 +1,42 @@
 package me.brunosantana.bdd.acceptance.steps;
 
+import java.math.BigDecimal;
+
+import org.junit.Assert;
+
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import me.brunosantana.bdd.model.Lance;
+import me.brunosantana.bdd.model.Leilao;
+import me.brunosantana.bdd.model.Usuario;
 
 public class PropondoLancesSteps {
 	
+	private Leilao leilao;
+	private Lance lance;
+	
+	@Before
+	public void setup() {
+		leilao = new Leilao("Tablet XPTO");
+	}
+	
 	@Given("Dado um lance valido")
 	public void dado_um_lance_valido() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		Usuario usuario = new Usuario("fulano");
+		lance = new Lance(usuario, BigDecimal.TEN);
 	}
 	
 	@When("Quando propoe ao leilao")
 	public void quando_propoe_ao_leilao() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		leilao.propoe(lance);
 	}
 	
 	@Then("Entao o lance eh aceito")
 	public void entao_o_lance_eh_aceito() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		Assert.assertEquals(1, leilao.getLances().size());
+		Assert.assertEquals(BigDecimal.TEN, leilao.getLances().get(0).getValor());
 	}
 
 }
