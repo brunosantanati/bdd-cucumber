0c9082bf044c68af77a6e72dbfa5846b8fa31efd
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2021-11-19 04:50:00 -0300

    Implementando steps para o novo cenario (varios lances validos).

--- bdd-cucumber/src/test/java/me/brunosantana/bdd/acceptance/steps/PropondoLancesSteps.java
+++ bdd-cucumber/src/test/java/me/brunosantana/bdd/acceptance/steps/PropondoLancesSteps.java
@@ -1,6 +1,7 @@
 package me.brunosantana.bdd.acceptance.steps;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 
 import org.junit.Assert;
 
@@ -16,9 +17,11 @@ public class PropondoLancesSteps {
 	
 	private Leilao leilao;
 	private Lance lance;
+	private ArrayList<Lance> lista;
 	
 	@Before
 	public void setup() {
+		this.lista = new ArrayList<Lance>();
 		leilao = new Leilao("Tablet XPTO");
 	}
 	
@@ -37,6 +40,24 @@ public class PropondoLancesSteps {
 	public void entao_o_lance_eh_aceito() {
 		Assert.assertEquals(1, leilao.getLances().size());
 		Assert.assertEquals(BigDecimal.TEN, leilao.getLances().get(0).getValor());
+	}
+	
+	@Dado("um lance de {double} reais do usuario {string}")
+	public void um_lance_de_x_reais_do_usuario_x(Double valor, String nomeUsuario) {
+		Lance lance = new Lance(new Usuario(nomeUsuario), new BigDecimal(valor));
+		lista.add(lance);
+	}
+	
+	@Quando("propoe varios lances ao leilao")
+	public void propoe_varios_lances_ao_leilao() {
+		this.lista.forEach(lance -> leilao.propoe(lance));
+	}
+
+	@Entao("os lances sao aceitos")
+	public void os_lances_sao_aceitos() {
+		Assert.assertEquals(this.lista.size(), leilao.getLances().size());
+		Assert.assertEquals(this.lista.get(0).getValor(), leilao.getLances().get(0).getValor());
+		Assert.assertEquals(this.lista.get(1).getValor(), leilao.getLances().get(1).getValor());
 	}
 
 }
