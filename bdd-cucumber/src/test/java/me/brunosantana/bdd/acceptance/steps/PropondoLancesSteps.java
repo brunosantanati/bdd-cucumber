859e0078a4886021ea460a2a46ef076e6349906b
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2021-11-19 04:39:27 -0300

    Usando palavras em portugues (atraves do Gherkin).

--- bdd-cucumber/src/test/java/me/brunosantana/bdd/acceptance/steps/PropondoLancesSteps.java
+++ bdd-cucumber/src/test/java/me/brunosantana/bdd/acceptance/steps/PropondoLancesSteps.java
@@ -5,9 +5,9 @@ import java.math.BigDecimal;
 import org.junit.Assert;
 
 import io.cucumber.java.Before;
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Entao;
+import io.cucumber.java.pt.Quando;
 import me.brunosantana.bdd.model.Lance;
 import me.brunosantana.bdd.model.Leilao;
 import me.brunosantana.bdd.model.Usuario;
@@ -22,18 +22,18 @@ public class PropondoLancesSteps {
 		leilao = new Leilao("Tablet XPTO");
 	}
 	
-	@Given("Dado um lance valido")
+	@Dado("um lance valido")
 	public void dado_um_lance_valido() {
 		Usuario usuario = new Usuario("fulano");
 		lance = new Lance(usuario, BigDecimal.TEN);
 	}
 	
-	@When("Quando propoe ao leilao")
+	@Quando("propoe ao leilao")
 	public void quando_propoe_ao_leilao() {
 		leilao.propoe(lance);
 	}
 	
-	@Then("Entao o lance eh aceito")
+	@Entao("o lance eh aceito")
 	public void entao_o_lance_eh_aceito() {
 		Assert.assertEquals(1, leilao.getLances().size());
 		Assert.assertEquals(BigDecimal.TEN, leilao.getLances().get(0).getValor());
--- bdd-cucumber/src/test/resources/features/propondo_lances.feature
+++ bdd-cucumber/src/test/resources/features/propondo_lances.feature
@@ -1,8 +1,10 @@
-Feature: Propondo lances ao leilao
+# language: pt
+
+Funcionalidade: Propondo lances ao leilao
 
 Pode escrever um texto aqui que serve de documentacao, mas sera ignorado pelo Cucumber
 
-Scenario: Propondo um unico lance valido
- Given Dado um lance valido
- When Quando propoe ao leilao
- Then Entao o lance eh aceito
+Cenario: Propondo um unico lance valido
+ Dado um lance valido
+ Quando propoe ao leilao
+ Entao o lance eh aceito
